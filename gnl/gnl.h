172545b3450cd9c485e0f4ca7e0e09adc1a7994b
Mihai Rusu <mrusu@3-E-8.42heilbronn.de>
2024-08-29 11:06:07 +0200

    last vers

--- gnl/gnl.h
+++ gnl/gnl.h
@@ -1,26 +1,15 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   gnl.h                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mrusu <mrusu@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/03 09:45:18 by mrusu             #+#    #+#             */
-/*   Updated: 2024/07/03 17:50:05 by mrusu            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GNL_H
-# define GNL_H
+#define GNL_H
 
-# include <unistd.h> // for read
-# include <stdlib.h> // for malloc
-# include <stdio.h> // for printf and End Of File
-# include <fcntl.h> // for open txt file
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+# define BUFFER_SIZE 42
 # endif
 
-char	*get_next_line(int fd);
+char *get_next_line(int fd);
 
 #endif
--- gnl/gnl2.c
+++ gnl/gnl2.c
@@ -2,61 +2,58 @@
 
 char *schr(char *s, int c)
 {
-	if (!s)
-		return (NULL);
+	if(!s)
+		return NULL;
 	while (*s)
 	{
-		if (*s ==(char)c)
+		if (*s == (char)c)
 			return (s);
 		s++;
 	}
-	return (NULL);
+	return NULL;
 }
-
 size_t slen(char *s)
 {
 	if(!s)
 		return 0;
 	size_t l = 0;
-	while(s[l])
+	while (s[l])
 		l++;
-	return (l);
+	return l;
 }
-
 void scpy(char *d, char *s)
 {
-	if (!d || !s)
+	if(!d || !s)
 		return ;
-	while (*s)
+	while(*s)
 		*d++ = *s++;
-	*d= '\0';
+	*d = '\0';
 }
-
 char *sdup(char *s)
 {
-	if (!s)
-		return (NULL);
+	if(!s)
+		return NULL;
 	size_t l = slen(s);
 	char *d = malloc(l+1);
-	if (!d)
-		return (NULL);
+	if(!d)
+		return NULL;
 	scpy(d, s);
 	return d;
 }
-char *join(char *s1, char *s2)
+char *sjoin(char *s1, char* s2)
 {
 	if(!s1 || !s2)
-		return (NULL);
+		return NULL;
 	size_t l1 = slen(s1);
 	size_t l2 = slen(s2);
-	char *d = malloc(l1+l2+1);
-	if(!d)
-		return (NULL);
-	scpy(d,s1);
-	scpy(d+slen(s1), s2);
+	char *j = malloc(l1+l2+1);
+	if(!j)
+		return NULL;
+	scpy(j,s1);
+	scpy(j+slen(s1), s2);
 	if(s1)
 		free(s1);
-	return (d);
+	return j;
 }
 
 char *get_next_line(int fd)
@@ -67,24 +64,25 @@ char *get_next_line(int fd)
 	int br;
 
 	line=sdup(buff);
-	if (!line)
-		return (NULL);
-	while(!(nline=schr(line, '\n')) && (br=read(fd, buff, BUFFER_SIZE)))
+	if(!line)
+		return NULL;
+	while(!(nline=schr(line, '\n')) && (br=read(fd,buff,BUFFER_SIZE)))
 	{
 		buff[br]='\0';
-		line =join(line, buff);
+		line=sjoin(line,buff);
 	}
 	if(slen(line) == 0)
 		return (free(line), NULL);
-	if(nline)
+	if (nline)
 	{
-		scpy(buff,nline+1);
+		scpy(buff, nline+1);
 		*(nline+1) = '\0';
 	}
 	else
-		buff[0]='\0';
-	return (line);
+		buff[0] = '\0';
+	return line;
 }
+
 
 int	main(void)
 {
